38e99fd70f5d971d61e3aa45c82c12c0b8aba97b
Reetro <thegamerboy7@gmail.com>
2019-11-02 16:05:39 -0600

    Added Files

---
+++ TripleX.cpp
@@ -0,0 +1,24 @@
+// This is my first c++ only project made during the second section the unreal c++ udemy course
+#include <iostream>
+
+int main()
+{
+    std::cout << "WE GOTTA GET SPONGEBOB BACK";
+    std::cout << std::endl;
+    std::cout << "SPONGEBOB IS JUST THROUGH THESE DOORS WE GOTTA GUESS THE RIGHT CODES TO FIND HIM";
+
+    const int a = 4;
+    const int b = 7;
+    const int c = 8;
+
+    const int sum = a + b + c; 
+    const int product = a * b * c; 
+
+    std::cout << std::endl;
+    std::cout << sum;
+    
+    std::cout << std::endl;
+    std::cout << product;
+
+    return 0;
+}
